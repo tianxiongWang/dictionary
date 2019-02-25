a67fa93189aae71b0c97e92cd18c35511d1d1064
wang <13961252501@139.com>
2019-02-25 07:53:30 -0800

    暂时终稿了，完成了不少功能，锻炼了自己的能力，加油！

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import time
 
 
 class Client(object):
@@ -10,9 +11,28 @@ class Client(object):
 		self.Socket.connect(self.addr)
 		self.Menu()
 	def sign_up(self):
-		pass
+		self.SendMessage('!!!!')                   #自定义解码格式，为!!!!，表示创建帐号请求
+		data = self.Socket.recv(1024).decode()
+		print(data)
+		username = input('请输入你的用户名:')
+		self.SendMessage('CCCC' + ' ' + username)  #自定义解码格式，CCCC代表发送的注册帐号
+		data = self.Socket.recv(1024)
+		if data == b'####':
+			print('用户名不重复，输入密码:')
+			password = input('请输入你的密码:')
+			self.SendMessage(password)  
+			print('注册完成，您现在可以登陆了!')
+		if data == b'!!!!':
+			print('用户名重复，已退出到主菜单!')
 	def sign_in(self):
-		pass
+		username = input('请输入用户名:')
+		password = input('请输入密码:')
+		self.SendMessage('SSSS' + ' '+ username + ' ' + password)  #自定义解码格式,SSSS代表申请登陆
+		data = self.Socket.recv(1024)
+		if data.decode() == 'OK':
+			self.Sec_Menu(username)
+		else:
+			print('登陆失败，用户名或密码错误!')
 	def Menu(self):
 		while True:
 			print('========================欢迎使用雄牌字典========================')
@@ -22,6 +42,7 @@ class Client(object):
 			print('************************3、退出词典****************************')
 			how = input()
 			if how == '3':
+				self.SendMessage('####')            #自定义解码格式，为####,表示退出
 				sys.exit('客户端已退出!')
 			elif how == '1':
 				self.sign_up()
@@ -30,24 +51,49 @@ class Client(object):
 			else:
 				print('您输入的命令有误，请重新输入!')
 				continue
-	def Sec_Menu(self):
+	def Search(self, username):
+		while True:
+			word = input('请输入你要查询的单词，输入##退出，注意，您的单词会被记录，请勿输入敏感词汇哟!\n在此输入:')
+			if word == '##':
+				break
+			self.SendMessage('MMMM' + ' ' + word + ' ' + username)                #自定义解码格式，表示查单词
+			data = self.Socket.recv(1024)
+			print(data.decode())
+			print()
+	def History(self, username):
+		data = 'HHHH ' + username
+		self.SendMessage(data)                   #自定义解码格式，表示查询历史记录
+		while True:
+			data = self.Socket.recv(1024)
+			if data == b'######':
+				break
+			else:
+				print(data.decode())
+	def Sec_Menu(self, username):
 		while True:
 			print('************************您已经成功登录了************************')
+			print('************************选择你需要的序号************************')
 			print('************************1、查询单词****************************')
 			print('************************2、查询记录****************************')
-			print('************************3、退出登录****************************')	
+			print('************************3、退出登录****************************')
+			how = input()				
 			if how == '3':
 				print('登陆已注销，欢迎您继续使用!')
 				break
 			elif how == '1':
-				self.sign_up()
+				self.Search(username)
 			elif how == '2':
-				self.sign_in()
+				self.History(username)
 			else:
 				print('您输入的命令有误，请重新输入!')
-				continue			
+				continue
+	def SendMessage(self, string):
+		time.sleep(0.1)
+		self.Socket.send(string.encode())
+
+
 def main():
-	addr = ('127.0.0.1',8421)
+	addr = ('127.0.0.1',8888)
 	B = Client(addr)
 	B.start()
 if __name__ == '__main__':
--- Server.py
+++ Server.py
@@ -1,16 +1,20 @@
 import socket
 import threading
 import pymysql
+import time
+import hashlib
+
 
 class db(object):
 	def __init__(self, db, user, password, addr):
-		self.db = pymysql.connect(addr, user, password, db)
-		self.cursor = self.db.cursor()
+		self.database = pymysql.connect(addr, user, password, db)
+		self.cursor = self.database.cursor()
 class Server(object):
-	def __init__(self, addr):
+	def __init__(self, addr, DB):
 		self.Socket = socket.socket()
 		self.Socket.bind(addr)
 		self.addr = addr
+		self.DB = DB                     #自定义该服务器类的数据库
 	def server_forever(self):
 		self.Socket.listen(5)
 		self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -25,12 +29,82 @@ class Server(object):
 		while True:
 			data = c.recv(1024)
 			if not data:
+				print(c.getpeername(),'已退出')
 				c.close()
 				break
+			if data == b'####':
+				print(c.getpeername(),'已退出')
+				break
+			if data == b'!!!!':
+				c.send('收到创建帐号请求!请开始你的表演!'.encode())
+			else:
+				msg = data.decode()
+				Req = msg.split(' ')               #接受信号，并进行解析
+				if Req[0] == 'CCCC':
+					self.DB.cursor.execute("select count(*) from customers where name = %s",[Req[1]])
+					x = self.DB.cursor.fetchone()
+					if x[0] == 0:
+						c.send(b'####')             #服务器的####表示没有重复用户名
+						data = c.recv(1024).decode()
+						s1 = hashlib.sha1()
+						s1.update(data.encode('utf8'))
+						password = s1.hexdigest()
+						sql = "insert into customers (name, password) values (%s, %s);"
+						self.DB.cursor.execute(sql, [Req[1], password])
+						self.DB.database.commit()
+					else:
+						c.send(b'!!!!')             #服务器的！！！！表示有重复用户
+				if Req[0] == 'SSSS':
+					username = Req[1]
+					password = Req[2]
+					s1 = hashlib.sha1()
+					s1.update(password.encode('utf8'))
+					password = s1.hexdigest()
+#					print(username,password)
+					self.DB.cursor.execute("select count(*) from customers where name = %s and password = %s",[username,password])
+					x = self.DB.cursor.fetchone()
+					if x[0] == 1:
+						c.send('OK'.encode())
+					if x[0] == 0:
+						c.send('No'.encode())
+				if Req[0] == 'MMMM':                 #此时Req[2]是用户名,Req[1]是查询的单词
+					word = Req[1]
+					username = Req[2]
+					sql = "select * from dictionary where word = %s"
+					self.DB.cursor.execute(sql, [word])
+					data = self.DB.cursor.fetchall()
+					mean = ''
+					for i in data:
+						mean = mean + '\n' + i[1]    #获得所有的意思
+					c.send(mean.encode())
+					sql = "select cust_id from customers where name = %s"
+					self.DB.cursor.execute(sql, [username])
+					cust_id = self.DB.cursor.fetchone()[0]
+					sql = "insert into log (date, cust_id, word) values (curtime(), %s, %s)"
+					self.DB.cursor.execute(sql, [cust_id, word])
+					self.DB.database.commit()
+				if Req[0] == 'HHHH':
+					sql = "select cust_id from customers where name = %s"
+					self.DB.cursor.execute(sql, [username])
+					cust_id = self.DB.cursor.fetchone()[0]
+					sql = "select * from log where cust_id = %s"
+					self.DB.cursor.execute(sql, [cust_id])
+					#返回多个值为元祖的元组，每个内部元组有4个值
+					history = self.DB.cursor.fetchall()
+					for i in history:
+						time.sleep(0.01)
+						data = "date:%s,word:%s" % (i[1], i[2])	
+						c.send(data.encode())
+					time.sleep(0.1)
+					c.send(b'######')     #自定义解码格式，表示不再发送记录
+				else:
+					continue
+
+
 def main():
-	addr = ('127.0.0.1',8421)
-	A = Server(addr)
+	addr = ('127.0.0.1',8888)
 	DB = db('dict', 'root', '931225', addr[0])
+	A = Server(addr, DB)
 	A.server_forever()
 if __name__ == '__main__':
 	main()
