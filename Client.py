e8b01e127a2af9427139f5d14a2557ef91d9e5cd
wang <13961252501@139.com>
2019-02-24 21:47:16 -0800

    添加基础通讯模块

--- Client.py
+++ Client.py
@@ -1,4 +1,19 @@
 import socket
 
-B = socket.socket()
-B.connect(('127.0.0.1',8888))
+class Client(object):
+	def __init__(self, addr):
+		self.Socket = socket.socket()
+		self.addr = addr
+	def start(self):
+		self.Socket.connect(self.addr)
+		self.Send()
+	def Send(self):
+		while True:
+			data = input('input')
+			self.Socket.send(data.encode())
+def main():
+	addr = ('127.0.0.1',8421)
+	B = Client(addr)
+	B.start()
+if __name__ == '__main__':
+		main()	
--- Server.py
+++ Server.py
@@ -1,4 +1,40 @@
 import socket
+import threading
+import pymysql
+
+class db(object):
+	def __init__(self, db, user, password, addr):
+		self.db = pymysql.connect(addr, user, password, db)
+		self.cursor = self.db.cursor()
 class Server(object):
 	def __init__(self, addr):
 		self.Socket = socket.socket()
+		self.Socket.bind(addr)
+		self.addr = addr
+	def server_forever(self):
+		self.Socket.listen(5)
+		self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.Thread_pool = []
+		while True:
+			c, addr = self.Socket.accept()
+			print('%s已连接' % str(addr))
+			t = threading.Thread(target = self.MultiThread, args = (c,))
+			t.daemon = True
+			self.Thread_pool.append(t)
+			t.start()
+	def MultiThread(self, *args):
+		c = args[0]
+		while True:
+			data = c.recv(1024)
+			if not data:
+				c.close()
+				break
+			#print(data.decode())
+			
+def main():
+	addr = ('127.0.0.1',8421)
+	A = Server(addr)
+	DB = db('dict', 'root', '931225', addr[0])
+	A.server_forever()
+if __name__ == '__main__':
+	main()
